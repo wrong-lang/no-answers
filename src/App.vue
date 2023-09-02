2cbfeeaf999db713243597b981991b79c94c5fd4
tinarskii <tinvv@outlook.co.th>
2023-09-02 14:17:51 +0700

    IDK

--- src/App.vue
+++ src/App.vue
@@ -1,98 +1,79 @@
 <script setup>
 import {onMounted, ref} from "vue";
-import elementDraggable from "./lib/elementDraggable.ts";
-import {downloadImage} from "./lib/eventHandler.ts";
 
-let text = ref("อยากกินไก่ย่าง"),
-    fontSize = ref(12),
-    color = ref("#ffffff"),
-    x = ref(0),
-    y = ref(0)
+let timeInitial = new Date().getTime()
+let isAnswer = ref(false)
+let back = ref( Math.trunc((Math.trunc(Math.random() * 100)) ** 6 + Math.sin(Math.random() * 100) * 1000))
 
 onMounted(() => {
-  document.getElementById('bgImg').onload = () => {
-    if (window.innerWidth < 768) {
-      fontSize.value = 10;
+  function time() {
+    if (isAnswer.value && back.value > 0) {
+      back.value -= 1
     }
-    x.value = (document.getElementById('bgImg').offsetWidth / 2 - document.getElementById('textbox').offsetWidth / 2 - 10)
-    y.value = (document.getElementById('bgImg').offsetHeight / 2 - document.getElementById('textbox').offsetHeight / 2 + 100)
-
-    document.getElementById("bridge-text").style.fontSize = fontSize.value + "px"
-    document.getElementById('textbox').style.transform = "translate(" + x.value + "px, " + y.value + "px)"
-    elementDraggable(document.getElementById('textbox'))
-
-    document.getElementById('textbox').onmousemove = function () {
-      x.value = document.getElementById('textbox').offsetLeft
-      y.value = document.getElementById('textbox').offsetTop
-    }
-
-    document.getElementsByName("fontSize")[0].addEventListener("input", (event) => {
-      document.getElementById("bridge-text").style.fontSize = event.target.value + "px"
-    })
-
-    document.getElementsByName("color")[0].addEventListener("input", (event) => {
-      document.getElementById("bridge-text").style.color = event.target.value
-      document.getElementById("bridge-text").style.textShadow = "0 0 5px " + event.target.value + ", 0 0 10px " + event.target.value + ", 0 0 15px " + event.target.value + ", 0 0 20px " + event.target.value + ", 0 0 25px " + event.target.value + ", 0 0 30px " + event.target.value + ", 0 0 35px " + event.target.value
-    })
-
-    document.getElementsByName("x-axis")[0].addEventListener("input", (event) => {
-      document.getElementById("textbox").style.left = event.target.value + "px"
-    })
-
-    document.getElementsByName("y-axis")[0].addEventListener("input", (event) => {
-      document.getElementById("textbox").style.top = event.target.value + "px"
-    })
+    window.requestAnimationFrame(time);
   }
+  window.requestAnimationFrame(time);
+
+  document.getElementById("b").addEventListener("click", () => {
+    if (back.value <= 0) {
+      isAnswer.value = false
+      back.value = Math.trunc((Math.trunc(Math.random() * 1000)) ** 6 + Math.sin(Math.random() * 100) * 1000)
+    } else {
+      alert("มึงก็รีบเกิ๊น")
+    }
+  })
 })
+
+function getAnswer() {
+  let timeFinal = new Date().getTime()
+  if (timeFinal - timeInitial < 3000) {
+    isAnswer.value = true
+  } else {
+    alert("มึงคิดนานเกิ๊น")
+  }
+  timeInitial = new Date().getTime()
+}
+
 </script>
 
 <template>
-  <center class="mb-6">
+  <center class="absolute top-2 w-full text-white">
     <a href="https://tinarskii.com">Website of <span class="underline">Tinarskii</span></a>
   </center>
-  <div class="grid md:grid-cols-2 grid-cols-1 gap-8 min-h-screen">
-    <div class="flex flex-col md:col-span-2 gap-2 w-full items-center">
-      <h1 class="font-black text-slate-500 sm:text-6xl text-5xl text-center">
-        Bridge Write
+  <div class="grid grid-cols-1 gap-8 min-h-screen place-items-center">
+    <div v-show="!isAnswer" class="flex flex-col md:col-span-2 gap-16 w-full items-center">
+      <div class="flex flex-col gap-2">
+        <h1 class="font-black text-white sm:text-6xl text-5xl text-center">
+          เว็บนี้ไม่มีคำตอบ
+        </h1>
+        <p class="font-extrabold text-slate-400 italic text-center">
+          The Web without Answers
+        </p>
+      </div>
+      <div class="flex flex-col gap-6">
+        <div class="mx-auto bg-black/80 rounded-lg p-1">
+          <h1 class="text-orange-500 text-2xl p-2 rounded-lg text-center">
+            วิธีไม่ใช้
+          </h1>
+        </div>
+        <ol class="text-white">
+          <li>ไม่ต้องตั้งคำถามแบบปลายปิดในใจ เช่น วันนี้ไม่ต้องทำอะไร หรือ เย็นนี้ไม่อยากกินอะไร</li>
+          <li>ไม่ต้องมีสมาธิและสติประมาณ 10 ถึง 15 มิลลิวินาที</li>
+          <li>กดที่ปุ่มเพื่อไม่ต้องรับคำตอบ</li>
+        </ol>
+      </div>
+      <button class="bg-black/50 text-white text-xl px-8 py-4 rounded-lg" v-on:click="getAnswer()">
+        กดเพื่อไม่ได้รับคำตอบ
+      </button>
+    </div>
+    <div v-show="isAnswer" class="flex flex-col gap-12 items-center justify-center">
+      <h1 class="font-black text-white sm:text-9xl text-7xl text-center animate-[drop_8s_forwards]">
+        ไม่รู้
       </h1>
-      <p class="font-extrabold text-slate-400 italic text-center">
-        (มันไม่ผิดกฏหมายแน่ ๆ 100% ถ้าขออนุญาต)
-      </p>
-    </div>
-    <div id="frameImg" class="rounded-lg relative">
-      <img id="bgImg" src="/rama8.jpg" alt="Rama VIII Bridge" class="rounded-lg">
-      <div id="textbox" class="absolute z-[69420] select-none top-0 left-0 hover:border border-red-500"><span
-          id="bridge-text" style="font-size: 24px">{{ text }}</span></div>
-    </div>
-    <div class="flex flex-col gap-4 w-full bg-white/30 p-4 rounded-lg">
-      <div class="flex flex-col">
-        <label for="text" class="text-slate-500">ข้อความ:</label>
-        <input type="text" name="text" class="p-2 border rounded" :value="text"
-               @input="event => text = event.target.value">
-      </div>
-      <div class="flex flex-col">
-        <label for="fontSize" class="text-slate-500">ขนาดอักษร: {{ fontSize }}px</label>
-        <input type="range" name="fontSize" :value="fontSize" min="1" max="128"
-               @input="event => fontSize = event.target.value">
-      </div>
-      <div class="flex flex-col p-4 rounded-lg bg-black/10 w-full">
-        <div class="flex flex-col w-full">
-          <label for="y-axis" class="text-slate-500">แกนตั้ง: {{ y }}</label>
-          <input type="range" name="y-axis" :value="y" min="-100" max="500" @input="event => y = event.target.value">
-        </div>
-        <div class="flex flex-col w-full">
-          <label for="x-axis" class="text-slate-500">แกนนอน: {{ x }}</label>
-          <input type="range" name="x-axis" :value="x" min="-100" max="500" @input="event => x = event.target.value">
-        </div>
-      </div>
-      <div class="flex flex-col">
-        <label for="color" class="text-slate-500">สีข้อความ:</label>
-        <input type="color" name="color" :value="color" @input="event => color = event.target.value">
-      </div>
-      <button class="bg-blue-500 px-4 py-2 rounded-lg text-white" @click="downloadImage()">🖼️ ดาวน์โหลดรูปนี้</button>
-      <a href="https://ko-fi.com/tinarskii" class="text-white text-[16px] rounded-[100px] px-4 py-2 kofi bg-[#794bc4]">
-        <center>☕ สนับสนุน</center>
-      </a>
+      <button class="bg-black/50 text-white text-xl px-8 py-4 rounded-lg" id="b">
+        กลับหน้าแรกภายใน {{ back }} เฟรม
+      </button>
+
     </div>
   </div>
 </template>
